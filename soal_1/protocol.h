12650cbce6960dd6183a6045789c103de0e793c3
22Trcky <sennabagusharimurti@gmail.com>
2026-05-03 15:09:34 +0700

    3

---
+++ soal_1/protocol.h
@@ -0,0 +1,10 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define PORT          12345
+#define MAX_CLIENTS   10
+#define BUFFER_SIZE   1024
+#define ADMIN_PASS    "protocol7"
+#define ALAMAT_SERVER "127.0.0.1"
+
+#endif
